c49c85134df8b27a4fccc7f8f3e78f398dabf245
Murilo Guizelin <muriloguizelin22@gmail.com>
2023-12-09 15:39:13 -0400

    falto

---
+++ frontend/app/src/Api.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const MyComponent = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Faça uma chamada GET para o endpoint do backend
+    axios.get('http://localhost:8080/filmes')
+      .then((response) => {
+        setData(response.data);
+      })    
+      .catch((error) => {
+        console.error('Erro ao obter dados do backend:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Dados do Backend:</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.nome}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MyComponent;
---
+++ frontend/app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MyComponent from './Api';
+
+function App() {
+  return (
+    <div>
+      <h1>Minha Aplicação React</h1>
+      <MyComponent />
+    </div>
+  );
+}
+
+export default App;
